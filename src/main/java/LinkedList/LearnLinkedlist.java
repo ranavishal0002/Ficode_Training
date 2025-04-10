afc8fca6c2e1e4d45860ddd043475ceca7fa1edb
VISHAL SINGH RANA <rvishalsingh0002@gmail.com>
2025-04-10 16:38:07 +0530

    init 4thrd commit

--- src/main/java/LinkedList/LearnLinkedlist.java
+++ src/main/java/LinkedList/LearnLinkedlist.java
@@ -2,14 +2,32 @@ package LinkedList;
 
 import java.util.LinkedList;
 
+
+// this is the Node of my linkedlist
+class Node {
+    int data;
+    Node next;
+    Node(int x){
+        data =x;
+        next= null;
+    }
+}
+// this is to create linkedlist
 public class LearnLinkedlist {
     public static void main(String[] args) {
-        LinkedList<String> linkedlist = new LinkedList<>();
-        linkedlist.add("vishal");
-        linkedlist.add("vikas");
-        linkedlist.add("diksha");
-        System.out.println(linkedlist);
-        System.out.println(linkedlist.getFirst());
-        System.out.println(linkedlist.getLast());
+       Node head = new Node(10);
+       head.next= new Node(20);
+       head.next.next = new Node(30);
+       head.next.next = new Node(40);
+       printlist(head);
+    }
+
+    // this is to print linked list
+    public static void printlist(Node head){
+        Node curr = head;
+        while(curr!=null){
+            System.out.println(curr.data);
+            curr= curr.next;
+        }
     }
 }
